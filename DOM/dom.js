92f8b5f649555a056757a067c6d69d9e9fbaeb7d
karthik <mailstominnu@gmail.com>
2023-01-24 23:31:12 +0530

    "dom and searching"

---
+++ DOM/dom.js
@@ -0,0 +1,12 @@
+//DOM (Document Object Model)
+// Load HTML -> Parse HTML -> DOM Tree
+// Head has title, meta and link while body has H1, div and section.
+// Inside div it has p button and a while section has article -> P -> Span
+
+        // Load HTML -> Parse HTML -> DOM --------------------->
+        //                 \>Load CSS -> parse CSS -> CSS OM --\
+        //                                                     \>Render Tree -> Visual formatting model
+
+
+        
+                                                    
